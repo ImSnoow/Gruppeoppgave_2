1eed0f2f5eee51430c943636e3f8854ce9670df1
ImSnoow <84009109+ImSnoow@users.noreply.github.com>
2021-05-26 13:55:25 +0200

    Added html, js and css for drag and drop

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Her lager vi en funksjon for når man begynner å dra elementet.
+
+function onDragStart(startDrag) {
+    startDrag.dataTransfer.setData('text', startDrag.target.id); //brukes for å lagre data fra der den stod, drar den over til et annet sted.
+    startDrag.currentTarget.style.backgroundColor = 'yellow';  //Oppdaterer stilen til elementet når man drar den.
+}
+
+// Event.preventDefault cancels an events default behaviour.
+// Vi gjør dette fordi <div> som regel ikke tillater drag and drop funksjoner. 
+function onDragOver(dragOver){
+    dragOver.preventDefault();
+}
+
+function onDrop(dropMe){
+    let dragging = dropMe.dataTransfer.getData('text');
+    let draggableElement = document.getElementById(dragging);
+    let dropzone = dropMe.target;
+    
+    dropzone.appendChild(draggableElement);
+    dropMe.dataTransfer.clearData();
+}
 
